7f776b9428e2ba60f2720eabe8591bc596a91e4d
oGre <ogre@muppfarmen.se>
2019-10-25 15:01:43 +0200

    Application

--- my-appsync-client/src/App.tsx
+++ my-appsync-client/src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import awsconfig from './aws-exports.js'
+import AWSAppSyncClient, {AUTH_TYPE} from 'aws-appsync/lib'
+import {ApolloProvider, DataProps, graphql} from 'react-apollo'
+import {Rehydrated} from 'aws-appsync-react/lib'
+import gql from 'graphql-tag'
 
-const App: React.FC = () => {
+// Här skapar vi en klient för AWS AppSync
+export const appSyncClient = new AWSAppSyncClient({
+  url: awsconfig.aws_appsync_graphqlEndpoint,
+  region: awsconfig.aws_appsync_region,
+  auth: {
+    type: AUTH_TYPE.API_KEY, // or type: awsconfig.aws_appsync_authenticationType,
+    apiKey: awsconfig.aws_appsync_apiKey,
+  }
+})
+
+// GraphQL-query för att läsa users
+const usersQuery = gql`query GetUsers {
+  users {
+    id
+    name
+  }
+}`
+
+// Komponent för att visa users-resultat
+const Users: React.FC<Partial<DataProps<any>>> = ({data}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <ul>
+        {((data && data.users) || []).map((u: any) => (
+            <li key={u.id}>{u.name}</li>
+        ))}
+      </ul>
+  )
 }
 
-export default App;
+// Koppla ihop komponenten ovan med graphql-klienten
+const UsersGraphQL = graphql(usersQuery)(Users)
+
+// Fancy app
+const App: React.FC = () => {
+  return (
+      <div>
+        <UsersGraphQL/>
+      </div>
+  )
+}
+
+// Komponent som kopplar ihop med Apollo och AppSync
+const WithApollo: React.FC = () => {
+  return (
+      <ApolloProvider client={appSyncClient}>
+        <Rehydrated>
+          <App/>
+        </Rehydrated>
+      </ApolloProvider>
+  )
+}
+
+// Observera att vi nu bytt till WithApollo som default-export
+export default WithApollo
